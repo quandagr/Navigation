97741d9b8c123ae043f387398519cd20ab2be413
Quanda Grant <quandagrant@gmail.com>
2026-02-10 22:21:46 -0500

    finishing adding touches to the photo

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
        <h1>Welcome to My Webpage</h1>
       {/* <p>This is a simple React application with routing.</p> */}
       {/* <p>Use the navigation links above to explore different pages.</p> */}
-      <img src="/nav.jpg" alt="Navigation System" width="100%"/>
+      <img src="/nav.jpg" alt="Navigation System" width="100%" height="480px" className="d-flex justify-content-center m-auto mt-5"/>
         </main>
 
 
